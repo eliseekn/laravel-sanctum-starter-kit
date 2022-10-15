fa62b71c3f33cc5f94267b9cc3f33fc53d8893fb
eliseekn <eliseekn@gmail.com>
2022-10-15 14:37:55 +0000

    Add UserRole.php Write UserRole enum unit tests

---
+++ app/Enums/UserRole.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Enums;
+
+enum UserRole: string
+{
+    case ADMIN = 'admin';
+    case USER = 'user';
+
+    public static function getValues(): array
+    {
+        return array_map(
+            fn ($case) => $case->value, self::cases()
+        );
+    }
+}
