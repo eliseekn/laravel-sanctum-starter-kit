6b37c8dba499b74ffc3922867366c9e8d0988168
eliseekn <eliseekn@gmail.com>
2022-10-15 14:38:03 +0000

    Add UserRole.php Write UserRole enum unit tests

---
+++ tests/Unit/UserRoleTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Enums\UserRole;
+use PHPUnit\Framework\TestCase;
+
+class UserRoleTest extends TestCase
+{
+    public function test_admin_role_value_is_correct()
+    {
+        $this->assertEquals('admin', UserRole::ADMIN->value);
+    }
+
+    public function test_user_role_value_is_correct()
+    {
+        $this->assertEquals('user', UserRole::USER->value);
+    }
+
+    public function test_roles_values_is_correct()
+    {
+        $this->assertEquals([
+            'admin',
+            'user'
+        ], UserRole::getValues());
+    }
+}
