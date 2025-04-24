a197028fc8d079e83f216046d2753311d2a00cab
eliseekn <eliseekn@gmail.com>
2025-04-24 11:14:51 +0000

    chore: add password rule

---
+++ app/Rules/CorrectPassword.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\Hash;
+
+class CorrectPassword implements ValidationRule
+{
+    /**
+     * @param  \Closure(string, ?string=): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $user = auth('sanctum')->user();
+
+        if (! Hash::check($value, $user?->password)) { // @phpstan-ignore-line
+            $fail('Incorrect password');
+        }
+    }
+}
