198859d626b444dcd27fc3bcfbea1f35078cadae
eliseekn <eliseekn@gmail.com>
2025-12-15 19:13:16 +0000

    chore

--- app/Http/UseCases/v1/User/GetCollectionUseCase.php
+++ app/Http/UseCases/v1/User/GetCollectionUseCase.php
@@ -14,21 +14,23 @@ final class GetCollectionUseCase
 {
     public function handle(array $query): AnonymousResourceCollection
     {
-        $query = User::query()
+        $result = User::query()
             ->when(! empty($query['startDate']) && ! empty($query['endDate']), function (Builder $q) use ($query) {
                 return $q->whereBetween(DB::raw('date(created_at)'), [$query['startDate'], $query['endDate']]);
             })
             ->when(! empty($query['search']), function (Builder $q) use ($query) {
-                return $q->where('prenoms', 'like', '%'.$query['search'].'%')
-                    ->orWhere('name', 'like', '%'.$query['search'].'%')
-                    ->orWhere('email', 'like', '%'.$query['search'].'%');
+                return $q->where(function ($subQuery) use ($query) {
+                    $subQuery->where('prenoms', 'like', '%'.$query['search'].'%')
+                        ->orWhere('name', 'like', '%'.$query['search'].'%')
+                        ->orWhere('email', 'like', '%'.$query['search'].'%');
+                });
             })
             ->orderBy('created_at', 'desc');
 
         if (! empty($query['perPage'])) {
-            $users = $query->paginate($query['perPage']);
+            $users = $result->paginate($query['perPage']);
         } else {
-            $users = $query->get();
+            $users = $result->get();
         }
 
         return UserResource::collection($users);
