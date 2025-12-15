3edc33c6104a5bf23f2943eb2a051f0b58eb2c09
eliseekn <eliseekn@gmail.com>
2025-12-15 19:13:13 +0000

    fix: columnn name

--- app/Http/Resources/v1/UserResource.php
+++ app/Http/Resources/v1/UserResource.php
@@ -18,7 +18,7 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
-            'role' => $this->password,
+            'role' => $this->role,
             'created_at' => $this->created_at,
         ];
     }
