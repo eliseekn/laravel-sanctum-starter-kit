3ab37aa10cae18c82c4750306002ff9090b1b4aa
eliseekn <eliseekn@gmail.com>
2026-08-15 18:14:48 +0000

    chore

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,16 +5,20 @@ namespace App\Models;
 use App\Enums\UserRole;
 use Database\Factories\UserFactory;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-#[Hidden([
+#[Fillable([
+    'name',
+    'email',
     'password',
-    'remember_token',
+    'role',
 ])]
+#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<UserFactory> */
